a89bd50225a10a2aca894e590f140da1192947e3
omairys <C0807503@mylambton.ca>
2021-08-06 13:10:42 -0400

    Update DataManager.java
    for word correction

--- app/src/main/java/com/example/bankapp/DataManager.java
+++ app/src/main/java/com/example/bankapp/DataManager.java
@@ -26,29 +26,29 @@ public class DataManager {
     }
 
     private Client initializeClient1() {
-        ArrayList<Account> acounts = new ArrayList<>();
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
 
-        return new Client("rick", "Rick Sanchez", acounts, "123456");
+        return new Client("rick", "Rick Sanchez", accounts, "123456");
     }
 
     private Client initializeClient2() {
-        ArrayList<Account> acounts = new ArrayList<>();
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
 
-        return new Client("morty", "Morty Smith", acounts, "123456");
+        return new Client("morty", "Morty Smith", accounts, "123456");
     }
 
     private Client initializeClient3() {
-        ArrayList<Account> acounts = new ArrayList<>();
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
-        acounts.add(new Account(789654321, 2505.0));
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
+        accounts.add(new Account(789654321, 2505.0));
 
-        return new Client("summer", "Summer Smith", acounts, "123456");
+        return new Client("summer", "Summer Smith", accounts, "123456");
     }
 }
