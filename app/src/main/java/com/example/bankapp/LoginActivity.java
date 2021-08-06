a311a4eab96dbab141b42a367c13c02d920d0f91
Tatiana <tanyaozerskaya@gmail.com>
2021-08-06 17:14:03 +0300

    Added some example code to main activity (for navigation)

--- app/src/main/java/com/example/bankapp/LoginActivity.java
+++ app/src/main/java/com/example/bankapp/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //todo chech validation and get Client from the ArrayList of clients
+                //only for test - added some client. Remove this row
+                Client client = new Client(123, "AA BB", "123R");
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("Client",client);
+                startActivity(intent);
+            }
+        });
     }
 }
