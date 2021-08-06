207a41e70f32081c8bf0d103fad1c3af3435bf98
Sheamus <c0807606@mylambton.ca>
2021-08-06 11:14:22 -0400

    Account List improvements.

--- app/src/main/java/com/example/bankapp/AccountListActivity.java
+++ app/src/main/java/com/example/bankapp/AccountListActivity.java
@@ -2,12 +2,36 @@ package com.example.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.bankapp.adapter.AccountListAdapter;
 
 public class AccountListActivity extends AppCompatActivity {
+
+    ListView listViewAccounts;
+
+    Account []accounts = {
+            new Account(1,100),
+            new Account(2, 1200),
+            new Account(3, 2000),
+            new Account(4, 4000)
+    };
+
+    AccountListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_list);
+
+        listViewAccounts = findViewById(R.id.listViewAccounts);
+
+        listAdapter = new AccountListAdapter(this, accounts);
+
+        listViewAccounts.setAdapter(listAdapter);
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/bankapp/adapter/AccountListAdapter.java
@@ -0,0 +1,42 @@
+package com.example.bankapp.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bankapp.Account;
+import com.example.bankapp.R;
+
+public class AccountListAdapter extends ArrayAdapter<Account> {
+    private Account[] accounts;
+    private Activity context;
+
+    public AccountListAdapter(Activity context, Account[] accounts) {
+        super(context, R.layout.list_item_account, accounts);
+        this.context = context;
+        this.accounts = accounts;
+
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.list_item_account, null, true);
+
+
+        TextView tvAccountName = (TextView) listViewItem.findViewById(R.id.textViewAccountId);
+        TextView tvBalance = (TextView) listViewItem.findViewById(R.id.textViewBalance);
+        ImageView image = (ImageView) listViewItem.findViewById(R.id.imageView);
+
+
+
+        tvAccountName.setText(accounts[position].getAccountId() + "");
+        tvBalance.setText(Double.toString(accounts[position].getAmount()));
+        return  listViewItem;
+    }
+}
